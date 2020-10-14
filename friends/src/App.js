adae21a00ad784c238b4a57760377129803bb603
Sam Lalli <2sam.lalli@gmail.com>
2020-10-13 21:43:55 -0600

    finished project

--- friends/src/App.js
+++ friends/src/App.js
@@ -2,7 +2,9 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 import './App.css';
 
+import FriendsList from './components/FriendsList';
 import Login from './components/Login'
+import PrivateRoute from './components/PrivateRoute'
 
 function App() {
   return (
@@ -12,9 +14,12 @@ function App() {
           <li>
           <Link to='/login'>Login</Link>
           </li>
+          <li>
+            <Link to='/protected'>Protected Page</Link>
+          </li>
         </ul>
         <Switch>
-          {/* PrivateRoute */}
+          <PrivateRoute exact path='/protected' component={FriendsList} />
           <Route path='/login' component={Login} />
         </Switch>
       </div>
--- friends/src/components/Login.js
+++ friends/src/components/Login.js
@@ -1,24 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
 import axios from 'axios'
 
 
 const Login = () => {
+
+    const [credentials, setCredentials] = useState({
+        username: '',
+        password: ''
+    })
+
+    const handleChanges = e => {
+        setCredentials({
+            ...credentials, [e.target.name]: e.target.value
+        })
+    }
+
+    const login = e => {
+        e.preventDefault();
+        axios
+            .post('http://localhost:5000/api/login', credentials)
+            .then((res)=> {
+                console.log(res)
+                window.localStorage.setItem('token', res.data.payload);
+                //this.history.push('/protected')
+            })
+            .catch(err => console.log(err))
+    }
+
     return(
         <div>
-            <form>
+            <form onSubmit={login}>
                 <label>Username: </label>
                 <input 
                     type='text'
                     name='username'
-                    //value
-                    //onchange
+                    value={credentials.username}
+                    onChange={handleChanges}
                 />
                 <label>Password: </label>
                 <input 
                     type='password'
                     name='password'
-                    //value
-                    //onchange
+                    value={credentials.password}
+                    onChange={handleChanges}
                 />
                 <button>Log in</button>
             </form>
